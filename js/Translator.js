bbb3de7dbe664237a5bb97bcbd5a43799868da50
Mohammed <mammegentleman@gmail.com>
2025-03-02 17:17:09 +0300

    Update Translator.js

--- js/Translator.js
+++ js/Translator.js
@@ -1,8 +1,14 @@
 const translator = document.querySelector(".translator");
-const language = document.querySelectorAll(".en, .am");
+const languageElements = document.querySelectorAll(".en, .am");
+
+const isTranslated = localStorage.getItem("translated") === "true";
+if (isTranslated) {
+    languageElements.forEach(text => text.classList.add("display"));
+}
 
 translator.addEventListener("click", () => {
-  language.forEach((text) => {
-    text.classList.toggle("display");
-  });
+    const isActive = languageElements[0].classList.toggle("display");
+    languageElements.forEach(text => text.classList.toggle("display"));
+    
+    localStorage.setItem("translated", isActive);
 });
